f6d92787a133d00d2ce8492a5de189f249b2a415
niklasApps <privat@nschroetler.de>
2018-05-12 14:50:13 +0200

    Added ids to selects

--- src/app/subject-table/subject-table.component.ts
+++ src/app/subject-table/subject-table.component.ts
@@ -7,12 +7,29 @@ declare let $: any;
 })
 export class SubjectTableComponent implements OnInit {
 
+  subjectCount = 1
+
   constructor() { }
 
   ngOnInit() {
     $(function () {
       $('select').material_select();
     });
+    this.initEventListeners();
   }
 
+  initEventListeners():void {
+    for(var i = 0; i < this.subjectCount; i++) {
+      
+    }
+  }
+
+  onSubjectChoiceChanged(semester:string, objid:string):void {
+    let choice = $("#"+objid).val();
+    console.log(choice);
+  }
+
+  setQ22withABI(abifachno):void {
+    console.log(abifachno);
+  }
 }
