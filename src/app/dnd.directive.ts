50d0c41cd12db1725218024d29d2cf79ee9d241c
Leo Maroni <leo@labcode.de>
2018-05-12 13:05:18 +0200

    modal open; dnd

--- src/app/dnd.directive.ts
+++ src/app/dnd.directive.ts
@@ -1,37 +1,39 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
-import {LupoService} from "./lupo.service";
+import {Directive, HostBinding, HostListener, Input} from '@angular/core';
+import {LupoService} from './lupo.service';
 
 @Directive({
   selector: '[appDnd]'
 })
 export class DndDirective {
 
-  @HostBinding('class') private classes = "";
+  @Input() appDnd: string;
+
+  private classes;
 
   constructor(private lupoService: LupoService) { }
 
-  @HostListener('dragover', ['$event']) onDragOver(evt){
+  @HostListener('dragover', ['$event']) onDragOver(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    let files = evt.dataTransfer.files;
-    if(files.length > 0){
-      this.classes = "drag-over";
+    const files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.classes = 'drag-over';
     }
   }
 
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt){
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.classes = "";
+    this.classes = '';
   }
 
-  @HostListener('drop', ['$event']) public onDrop(evt){
+  @HostListener('drop', ['$event']) public onDrop(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    let files = evt.dataTransfer.files;
-    if(files.length === 1){
+    const files = evt.dataTransfer.files;
+    if (files.length === 1) {
       this.lupoService.convertLupoFile(files[0]);
     }
-    this.classes = "";
+    this.classes = '';
   }
 }
